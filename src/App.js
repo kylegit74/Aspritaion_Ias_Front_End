354f5ad6da263d07188507d7f2d051adcf64b991
kylegit74 <kylefront74@gmail.com>
2025-02-13 20:29:22 +0530

    added login ui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WhyChooseUs from '../src/pages/WhyChooseUs'
 import AdminRoute from './components/AdminPanel/Layouts/AdminRoute';
 
+import Login from './components/Login/Login';
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,7 @@ function App() {
         <Route path='/about' element={<AboutUs />} />
         <Route path='/contact' element={<ContactPage/>}/>
         <Route path='/whychooseus' element={<WhyChooseUs/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path="/admin/*" element={<AdminRoute />} />
       </Routes>
     </BrowserRouter>
